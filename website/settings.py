3a9c719d63800125707d8f20b69f69b8e0b6de2c
suhasjadhav23 <suhasjadhav23@gmail.com>
2017-09-28 07:53:44 +0530

    dj_static added

--- website/settings.py
+++ website/settings.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-import dj_database_url
-
 
 import os
 
@@ -81,22 +79,16 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         'USER': '',
-#         'PASSWORD': '',
-#         'HOST': '',
-#         'PORT': '',
-# }
-# }
-
-  DATABASES = {
-      'default': dj_database_url.config(
-          default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-      )
-  }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+}
+}
 ACCOUNT_ACTIVATION_DAYS = 7
 
 # Password validation
--- website/wsgi.py
+++ website/wsgi.py
@@ -7,10 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
+
+# application = get_wsgi_application()
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
