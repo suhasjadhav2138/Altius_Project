f01d4fb288ee9b82a5224fc4f16198d028ec84f6
suhasjadhav23 <suhasjadhav23@gmail.com>
2017-10-30 19:33:01 +0530

    model created for search details

--- login/admin.py
+++ login/admin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from .models import UserProfilename, Document
+from .models import UserProfilename, Document, Search_details
 from django.contrib import admin
 
 admin.site.register(UserProfilename)
 admin.site.register(Document)
+admin.site.register(Search_details)
