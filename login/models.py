f4988d12307fac97065c492ce33236c0ef910e34
suhasjadhav23 <suhasjadhav23@gmail.com>
2017-11-07 20:58:31 +0530

    removed sessions

--- login/models.py
+++ login/models.py
@@ -48,19 +48,19 @@ class UserSession(models.Model):
     #     return unicode(self.sessio)
    
 
-    def user_logged_in_handler(sender, request, user, **kwargs):
-        print("ooooooooooooooooooooooooooooooooooooooooooooooooo")
-        data= get_ip_location()
-        ip = data['ip']
-        location = data['city']+', '+data['region'] + ', '+ data['country']
-        UserSession.objects.get_or_create(
-            user = user,
-            session_id = request.session.session_key,
-            ip=ip,
-            location=location
-        )
+    # def user_logged_in_handler(sender, request, user, **kwargs):
+    #     print("ooooooooooooooooooooooooooooooooooooooooooooooooo")
+    #     data= get_ip_location()
+    #     ip = data['ip']
+    #     location = data['city']+', '+data['region'] + ', '+ data['country']
+    #     UserSession.objects.get_or_create(
+    #         user = user,
+    #         session_id = request.session.session_key,
+    #         ip=ip,
+    #         location=location
+    #     )
 
-    user_logged_in.connect(user_logged_in_handler)
+    # user_logged_in.connect(user_logged_in_handler)
 
 def get_ip_location():
     url = 'http://ipinfo.io/json'
